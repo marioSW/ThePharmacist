8ad87eb903052c983f835eb4c8c45b5ba7460651
Roshin <roshinhh@gmail.com>
2016-03-23 21:34:55 +0530

    recover pass 1

--- app/src/main/java/com/freeoda/pharmacist/thepharmacist/Recover_password.java
+++ app/src/main/java/com/freeoda/pharmacist/thepharmacist/Recover_password.java
@@ -1,22 +1,71 @@
 package com.freeoda.pharmacist.thepharmacist;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class Recover_password extends AppCompatActivity {
 
-    EditText email = (EditText)findViewById(R.id.reset_email);
+    //EditText email = (EditText)findViewById(R.id.reset_email);
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String>{
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            StringBuffer chaine = new StringBuffer("");
+            String response = "";
+            try {
+                URL url = new URL("http://pharmacist2016.netau.net/reset_password.php");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestProperty("User-Agent", "");
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write("mail=hi");
+
+                writer.flush();
+                writer.close();
+                os.close();
+                int responseCode=connection.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+                    String line;
+                    BufferedReader br=new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    while ((line=br.readLine()) != null) {
+                        response+=line;
+                    }
+                }
+                else {
+                    response="";
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //return response;
+
+            Log.d("eeeeeee", response);
+            Log.d("dddddddd", "------------------------------------------------");
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,29 +77,7 @@ public class Recover_password extends AppCompatActivity {
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringBuffer chaine = new StringBuffer("");
-                try{
-                    URL url = new URL("http://pharmacist2016.netau.net/reset_password.php");
-                    HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-                    connection.setRequestProperty("User-Agent", "");
-                    connection.setRequestMethod("POST");
-                    connection.setDoInput(true);
-                    connection.connect();
-
-                    InputStream inputStream = connection.getInputStream();
-
-                    BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
-                    String line = "";
-                    while ((line = rd.readLine()) != null) {
-                        chaine.append(line);
-                    }
-
-                } catch (IOException e) {
-                    // writing exception to log
-                    e.printStackTrace();
-                }
-
-                Log.d("eeeeeee",chaine.toString());
+                new RetrieveFeedTask().execute();
             }
         });
     }
